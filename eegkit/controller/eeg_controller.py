b8e7378e055ce9a73ddf86cfa4b33f383415a1b5
Nantawat6510545543 <nantawat.suk@ku.th>
2025-08-07 05:55:02 +0000

    refactor: split EEGVisualization to EEGDataService, flat registry, and controller-grouped spec

--- eegkit/controller/eeg_controller.py
+++ eegkit/controller/eeg_controller.py
@@ -1,7 +1,7 @@
 from ..models import (
     FilterParamsDTO, EpochParamsDTO, TaskDTO
 )
-from ..services import EEGVisualization
+from ..services import EEGVisualization, EEGDataService
 
 class EEGController:
     def __init__(self, subject_model):
@@ -11,14 +11,22 @@ class EEGController:
             get_epochs_func=self.get_epochs,
             get_task_func=self.subject_model.get_task
         )
+        self.data_service = EEGDataService(
+            get_raw_func=self.get_filtered_raw,
+            get_epochs_func=self.get_epochs,
+            get_task_func=self.subject_model.get_task
+        )
+
+        self.specs =  {
+            "plot": self.visualizer.spec,
+            "data": self.data_service.spec,
+        }
 
     def get_filtered_raw(self, task_dto: TaskDTO, filter_params: FilterParamsDTO):
-        # print("Getting raw")
         task_model = self.subject_model.get_task(task_dto)
         return task_model.get_filtered_raw(filter_params)
 
     def get_epochs(self, task_dto: TaskDTO, epoch_params: EpochParamsDTO):
-        # print("Getting epochs")
         task_model = self.subject_model.get_task(task_dto)
         return task_model.get_epochs(epoch_params)
 
@@ -27,16 +35,18 @@ class EEGController:
 
     def list_tasks(self, subject):
         return self.subject_model.list_tasks(subject)
-
+    
     def get_specs(self):
-        return self.visualizer.specs
+        return self.specs
     
     def prepare(self, task_dto: TaskDTO, group: str, key: str):
-        return self.visualizer.prepare_params(task_dto, group, key)
+        if group == "plot":
+            return self.visualizer.prepare_params(task_dto, key)
+        else:
+            return {}
     
     def show(self, task_dto: TaskDTO, group: str, key: str, params_dto):
-        # print("Visualizing")
-        result = self.visualizer.specs[group][key]["function"](task_dto, params_dto)
+        result = self.specs[group][key]["function"](task_dto, params_dto)
         return result
 
 
--- eegkit/services/visualization.py
+++ eegkit/services/visualization.py
@@ -1,17 +1,16 @@
-import pandas as pd
 from ..models import (
     TaskDTO, FilterParamsDTO, TimeDomainParamsDTO, PSDParamsDTO,
     EpochParamsDTO, EpochFullParamsDTO, TableInfoDTO, EpochPSDParamsDTO
 )
 from ..utils import finalize_figure
-from copy import deepcopy
 import matplotlib.pyplot as plt
 plt.ioff()
 
-plot_registry = {"Plot": {}, "Data": {}}
-def register_plot(group, name, dto_cls):
+plot_registry = {}
+
+def register_plot(name, dto_cls):
     def decorator(func):
-        plot_registry[group][name] = {
+        plot_registry[name] = {
             "params": dto_cls,
             "function": func
         }
@@ -24,37 +23,32 @@ class EEGVisualization:
         self.get_raw = get_raw_func
         self.get_epochs = get_epochs_func
         self.get_task = get_task_func
-        self.specs = plot_registry
+        self.spec = plot_registry
+        for key in self.spec:
+            func = self.spec[key]["function"]
+            self.spec[key]["function"] = func.__get__(self)
 
-        for group in self.specs:
-            for key in self.specs[group]:
-                func = self.specs[group][key]["function"]
-                self.specs[group][key]["function"] = func.__get__(self)
-
-    def prepare_params(self, task_dto: TaskDTO, group: str, key: str):
-        spec = self.specs[group][key]
+    def prepare_params(self, task_dto: TaskDTO, key: str):
+        spec = self.spec[key]
         params = spec["params"]
         if params is None:
             return 
         updates = {}
-        # print(f"preparing with {params} as type {type(params)}")
         if isinstance(params(), EpochParamsDTO):
             epochs, labels = self.get_epochs(task_dto, params)
-            # print(labels)
             if labels is not None:
                 updates["stimulus"] = [None] + sorted(set(labels))
             else:
                 updates["stimulus"] = [None] 
 
-        # print("returning")
         return updates
 
-    @register_plot("Plot", "Sensor Layout", FilterParamsDTO)
+    @register_plot("Sensor Layout", FilterParamsDTO)
     def plot_sensors(self, task_dto: TaskDTO, params: FilterParamsDTO):
         raw = self.get_raw(task_dto, params)
         raw.plot_sensors(show_names=True)
 
-    @register_plot("Plot", "Time Domain Plot", TimeDomainParamsDTO)
+    @register_plot("Time Domain Plot", TimeDomainParamsDTO)
     def plot_time(self, task_dto: TaskDTO, params: TimeDomainParamsDTO):
         raw = self.get_raw(task_dto, params)
         fig = raw.plot(
@@ -66,14 +60,14 @@ class EEGVisualization:
         )
         return [finalize_figure(fig, task_dto, caption=vars(params), plot_name="Time Domain")]
 
-    @register_plot("Plot", "Frequency Domain", PSDParamsDTO)
+    @register_plot("Frequency Domain", PSDParamsDTO)
     def plot_frequency(self, task_dto: TaskDTO, params: PSDParamsDTO):
         raw = self.get_raw(task_dto, params)
         psd = raw.compute_psd(fmin=params.fmin, fmax=params.fmax)
         fig = psd.plot(average=params.average, spatial_colors=params.spatial_colors, dB=params.dB, show=False)
         return [finalize_figure(fig, task_dto, caption=vars(params), plot_name="Frequency Domain")]
 
-    @register_plot("Plot", "Condition-wise PSD", EpochPSDParamsDTO)
+    @register_plot("Condition-wise PSD", EpochPSDParamsDTO)
     def plot_conditionwise_psd(self, task_dto: TaskDTO, params: EpochPSDParamsDTO):
         epochs, labels = self.get_epochs(task_dto, params)
         if epochs is None:
@@ -90,11 +84,11 @@ class EEGVisualization:
             fig_list.append(fig)
         return fig_list
 
-    @register_plot("Plot", "Epoch Plot", EpochFullParamsDTO)
+    @register_plot("Epoch Plot", EpochFullParamsDTO)
     def plot_epochs(self, task_dto: TaskDTO, params: EpochParamsDTO):
         return self._plot_epochs_base(task_dto, params, mode="Epochs")
 
-    @register_plot("Plot", "Evoked Response", EpochParamsDTO)
+    @register_plot("Evoked Response", EpochParamsDTO)
     def plot_evoked(self, task_dto: TaskDTO, params: EpochParamsDTO):
         return self._plot_epochs_base(task_dto, params, mode="Evoked")
 
@@ -112,55 +106,3 @@ class EEGVisualization:
             fig = epochs.average().plot_joint(show=False)
 
         return [finalize_figure(fig, task_dto, params.stimulus, caption=vars(params), plot_name=mode)]
-
-    @register_plot("Data", "EEG Table", TableInfoDTO)
-    def show_table(self, task_dto: TaskDTO, table_info: TableInfoDTO):
-        task_model = self.get_task(task_dto)
-        df_map = {
-            'events': task_model.events,
-            'channels': task_model.channels,
-            'electrodes': task_model.electrodes
-        }
-        return df_map.get(table_info.table_type, pd.DataFrame()).head(table_info.rows)
-
-    @register_plot("Data", "Epochs Table", EpochParamsDTO)
-    def show_epochs_table(self, task_dto: TaskDTO, params: EpochParamsDTO):
-        epochs, labels = self.get_epochs(task_dto, params)
-        if epochs is None or labels is None:
-            return None
-
-        labels_series = pd.Series(labels)
-        unique_labels = sorted(set(labels_series))
-        rows = []
-
-        for label in unique_labels:
-            # Find indices for this label
-            idx = labels_series[labels_series == label].index
-            label_epochs = epochs[idx]
-            row = {
-                'label': label,
-                'n_epochs': len(label_epochs),
-                'n_channels': len(label_epochs.ch_names),
-                'timespan_sec': label_epochs.times[-1] - label_epochs.times[0] if len(label_epochs.times) > 1 else 0,
-                'sampling_rate': label_epochs.info['sfreq'],
-                'duration_per_epoch_sec': label_epochs.get_data().shape[-1] / label_epochs.info['sfreq']
-            }
-            rows.append(row)
-
-        return pd.DataFrame(rows)
-
-    @register_plot("Data", "Annotations", FilterParamsDTO)
-    def get_annotation_df(self, task_dto: TaskDTO, filter_params: FilterParamsDTO):
-        raw = self.get_raw(task_dto, filter_params)
-        annots = raw.annotations
-        df = pd.DataFrame({
-            "onset": annots.onset,
-            "duration": annots.duration,
-            "description": annots.description
-        })
-        return df
-
-    @register_plot("Data", "Metadata", None)
-    def show_annotations(self, task_dto: TaskDTO, params: FilterParamsDTO):
-        task_model = self.get_task(task_dto)
-        return task_model.metadata
