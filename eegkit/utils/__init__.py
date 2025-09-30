84739037cc64b4a724cf60070b104fec35b25eaf
Nantawat6510545543 <nantawat.suk@ku.th>
2025-09-30 16:30:29 +0700

    Add: Introduce plot_utills module with utility functions for tokenization and evoked response visualization

--- eegkit/utils/__init__.py
+++ eegkit/utils/__init__.py
@@ -1,1 +1,8 @@
 from .figure_utils import finalize_figure
+from .plot_utills import (
+    split_tokens,
+    compute_axes_values,
+    map_cells_to_labels,
+    reshape_axes_array,
+    draw_evoked_response,
+)
